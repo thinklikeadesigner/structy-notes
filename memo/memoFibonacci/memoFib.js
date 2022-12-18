412ff16d1e24fb58fb56db4cf757a6f39399149c
Rebecca Burch <rebecca.burch.stu@gmail.com>
2022-12-17 19:29:26 -0600

    second commit

---
+++ memo/memoFibonacci/memoFib.js
@@ -0,0 +1,12 @@
+// js object, keys will be arg to fn, value will be the return // value
+const fib = (n, memo = {}) => {
+  if (n in memo) return memo[n];
+  if (n <= 2) return 1;
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
+};
+
+console.log(fib(6)); // 8
+console.log(fib(7)); // 13
+console.log(fib(8)); // 21
+console.log(fib(50)); // runs almost instantly
